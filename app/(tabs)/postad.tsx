e7c1e1b7f70f6fb9a7ea80329b5ba76ca4cf233c
Azovayt <aziz.beyaz@outlook.com>
2025-06-02 23:11:25 +0300

    yenileme

--- app/(tabs)/postad.tsx
+++ app/(tabs)/postad.tsx
@@ -1,14 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import CustomHeader from '../../src/components/CustomHeader'
+import { View, Text } from "react-native";
+import React from "react";
+import CustomHeader from "../../src/components/CustomHeader";
 
 const PostAd = () => {
   return (
     <View>
-      <CustomHeader title='Post Ad' logo={true}/>
-      <Text>PostAdScreen</Text>
+      <CustomHeader title="Post Ad" logo={true} />
+      <Text className="text-2xl font-bold">PostAdScreen</Text>
     </View>
-  )
-}
+  );
+};
 
 export default PostAd;
